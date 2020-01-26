aa1d733af1ef9e2475e15014bdacdbf880a7c966
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-01-25 18:23:38 -0600

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Ricardo González Castillo on 1/25/20.
+//  Copyright © 2020 Ricardo González Castillo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
