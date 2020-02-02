c5c7450cd838ded5c890fde3fb424f804fc3946d
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-02-01 18:14:39 -0600

    Add GraphQL to the project

---
+++ Pokedex/Data/NetworkingManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkingManager.swift
+//  Pokedex
+//
+//  Created by Ricardo J. González on 01/02/20.
+//  Copyright © 2020 Ricardo González Castillo. All rights reserved.
+//
+
+import Foundation
